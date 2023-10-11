cc15e9fbf9e92280a602fbe2489003eda3061669
Cooper Brien <cooper.brien@snhu.edu>
2023-10-11 11:48:54 -0400

    Added Google Authentication
    Google authentication is working, after pulling need to edit .env to add GOOGLE_CLIENT_ID And GOOGLE_SECRET_ID

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,10 +7,12 @@ import {
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 import { getSession, useSession } from "next-auth/react";
+import { Session } from "inspector";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -40,7 +42,24 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session: ({ session, user }) => ({
+    /*async signIn({user, account, profile, email, credentials}){
+      const isAllowedToSignIn = true
+      if(isAllowedToSignIn){
+        return true
+      }
+      else{
+        return false
+      }
+    },*/
+    
+    session({session, user}) {
+      strategy: "jwt"
+      if(session.user) {
+        session.user.id = user.id
+      }
+      return session
+    },
+    /*session: ({ session, user }) => ({
       strategy: "jwt",
       ...session,
       user: {
@@ -48,13 +67,17 @@ export const authOptions: NextAuthOptions = {
         id: user.id,
       },
       
-    }),
+    }),*/
   },
   adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     CredentialsProvider({
       name: "Credentials",
@@ -69,7 +92,6 @@ export const authOptions: NextAuthOptions = {
         const user = {id: "1", name: "CoopDaScoop", email: "n/a", password: "Coopy"}
 
         if (user.name == credentials!.username && user.password == credentials!.password) {
-          
           return user
         } else{
           return null
@@ -82,7 +104,7 @@ export const authOptions: NextAuthOptions = {
      * ...add more providers here.
      *
      * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+     * GitHub provider requires you to add the `refresh__expires_in` field to the Account
      * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
      *
      * @see https://next-auth.js.org/providers/github
