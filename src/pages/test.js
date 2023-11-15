4b73d7a92b76260726cdb8e28a61bda72ee85b4f
Cooper Brien <cooper.brien@snhu.edu>
2023-11-15 11:36:01 -0500

    Updated Seed.ts

--- src/pages/test.js
+++ src/pages/test.js
@@ -8,7 +8,7 @@ const TestPage = () => {
     return (
       <div className={`${styles["bg-image2"]} ${styles["your-other-styles"]}`}>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', padding: '0 20px', color: 'white' }}>
-          <h1 style={{ fontSize: '20px', color: 'red', fontWeight: 'bold' }}>About Us</h1>
+          <h1 style={{ fontSize: '20px', color: 'red', fontWeight: 'bold' }}>TESTING STAT DISPLAY</h1>
           <p style={{ textAlign: 'center' }}>
               <div><UserInfo /></div>
           </p>
