97d6261294532a168305943a66f205081cc82e08
Cooper Brien <cooper.brien@snhu.edu>
2023-12-06 11:20:11 -0500

    Edited Seed and prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,18 +3,17 @@ const prisma = new PrismaClient()
 
 async function main(){
     const user_test = await prisma.mcuser.upsert({
-        where: {UUID: '1234'},
+        where: {uuid: '1234'},
         update: {},
         create: {
-            UUID: '1234',
+            uuid: '1234',
             username: 'CoopDaScoop32',
             worlds:{
               create:{
                 world_name: 'New_World',
-                date_created: '10/17/12',
                 stats: {
                   create: {
-                    UUID: '1234',
+                    uuid: '1234',
                     stat_name: 'test_stat',
                     stat_val: '5'
 
@@ -27,23 +26,22 @@ async function main(){
     console.log({user_test})
 
     const user_test2 = await prisma.mcuser.upsert({
-      where: {UUID: '4321'},
+      where: {uuid: '4321'},
       update: {},
       create: {
-          UUID: '4321',
+          uuid: '4321',
           username: 'ScoopTheCoop23',
           worlds:{
             create:[{
               world_name: 'My_World',
-              date_created: '3/28/20',
               stats: {
                 create: [{
-                  UUID: '4321',
+                  uuid: '4321',
                   stat_name: 'chickens_killed',
                   stat_val: '3'
                 },
                 {
-                  UUID: '4321',
+                  uuid: '4321',
                   stat_name: 'deaths',
                   stat_val: '204'
                 }],
@@ -52,15 +50,14 @@ async function main(){
             },
             {
               world_name: 'World2',
-              date_created: '10/31/09',
               stats:{
                 create:[{
-                  UUID: '4321',
+                  uuid: '4321',
                   stat_name:'chickens_killed',
                   stat_val:'20'
                 },
                 {
-                  UUID: '4321',
+                  uuid: '4321',
                   stat_name:'deaths',
                   stat_val:'64'
                 }],
