a44cf1228c181cf114b02949a5b37ecdc35e42c0
kstone4216 <kstone0824@gmail.com>
2023-12-06 11:27:00 -0500

    uuid

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,10 +3,10 @@ const prisma = new PrismaClient()
 
 async function main(){
     const user_test = await prisma.mcuser.upsert({
-        where: {UUID: '1234'},
+        where: {uuid: '1234'},
         update: {},
         create: {
-            UUID: '1234',
+            uuid: '1234',
             username: 'CoopDaScoop32',
             worlds:{
               create:{
@@ -14,7 +14,7 @@ async function main(){
                 date_created: '10/17/12',
                 stats: {
                   create: {
-                    UUID: '1234',
+                    uuid: '1234',
                     stat_name: 'test_stat',
                     stat_val: '5'
 
@@ -27,7 +27,7 @@ async function main(){
     console.log({user_test})
 
     const user_test2 = await prisma.mcuser.upsert({
-      where: {UUID: '4321'},
+      where: {uuid: '4321'},
       update: {},
       create: {
           UUID: '4321',
@@ -38,12 +38,12 @@ async function main(){
               date_created: '3/28/20',
               stats: {
                 create: [{
-                  UUID: '4321',
+                  uuid: '4321',
                   stat_name: 'chickens_killed',
                   stat_val: '3'
                 },
                 {
-                  UUID: '4321',
+                  uuid: '4321',
                   stat_name: 'deaths',
                   stat_val: '204'
                 }],
@@ -52,15 +52,14 @@ async function main(){
             },
             {
               world_name: 'World2',
-              date_created: '10/31/09',
               stats:{
                 create:[{
-                  UUID: '4321',
+                  uuid: '4321',
                   stat_name:'chickens_killed',
                   stat_val:'20'
                 },
                 {
-                  UUID: '4321',
+                  uuid: '4321',
                   stat_name:'deaths',
                   stat_val:'64'
                 }],
