c4d498caf36213fd2a332584b73c16b322e6d743
averwhy <averwhy@gmail.com>
2023-12-12 16:59:42 +0000

    kinda need this file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,11 +10,16 @@ const config = {
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
+  ignorePatterns: ["prismaTest.js", "RetrieveInfo.js", "about.js", "userData.js", "contact.js", "main.js", "test.js"],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
